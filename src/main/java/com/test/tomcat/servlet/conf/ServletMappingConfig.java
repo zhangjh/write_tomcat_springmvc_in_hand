caeeb4b1c5f2c2a6957103b3ad715a23cca296f8
zhangjh <njhxzhangjihong@126.com>
2019-03-20 15:19:46 +0800

    update

--- src/main/java/com/test/tomcat/servlet/conf/ServletMappingConfig.java
+++ src/main/java/com/test/tomcat/servlet/conf/ServletMappingConfig.java
@@ -3,6 +3,9 @@ package com.test.tomcat.servlet.conf;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.test.tomcat.servlet.servlet.MyServlet1;
+import com.test.tomcat.servlet.servlet.MyServlet2;
+
 /**
  * @author : zhangjh
  * @date : 2019-03-19
@@ -13,7 +16,7 @@ public class ServletMappingConfig {
     public static final List<ServletMapping> SERVLET_MAPPINGS = new ArrayList<ServletMapping>();
 
     static {
-        SERVLET_MAPPINGS.add(new ServletMapping("Test1", "/test1", "com.test.tomcat.servlet.servlet.MyServlet1"));
-        SERVLET_MAPPINGS.add(new ServletMapping("Test2", "/test2", "com.test.tomcat.servlet.servlet.MyServlet2"));
+        SERVLET_MAPPINGS.add(new ServletMapping(MyServlet1.class.getSimpleName(), "/test1", MyServlet1.class.getName()));
+        SERVLET_MAPPINGS.add(new ServletMapping(MyServlet2.class.getSimpleName(), "/test2", MyServlet2.class.getName()));
     }
 }
